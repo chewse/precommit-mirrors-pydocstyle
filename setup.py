52ebf928f40c5344b8d81d259ddb1bb4e96ffe8a
Daniel Ward <d@d-w.me>
2017-08-02 13:11:46 +0100

    Support version pydocstyle 2.0.0

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 
 setup(
     name='pre-commit-mirrors-pydocstyle',
-    version='0.0.0',
-    install_requires=['pydocstyle==1.0.0'],
+    version='2.0.0',
+    install_requires=['pydocstyle==2.0.0'],
 )
